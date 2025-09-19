45247abda1b9f0e5169fe6866ee9ac456b3ebe4d
KazikKluz <kazik.kluz@gmail.com>
2025-09-19 12:58:49 +0100

    feat: add recursive copy function to move files from static directory to public

--- src/main.py
+++ src/main.py
@@ -1,11 +1,40 @@
-from src.textnode import TextNode, TextType
+
+"""This is the starting module"""
+
+import os
+import shutil
 
 
 def main():
-    node = TextNode("This is some anchor text",
-                    TextType.LINK, "https://www.boot.dev")
+    """Main function that executes the entire application"""
 
-    print(node)
+    if not os.path.exists("./static"):
+        raise FileNotFoundError("The ./static path does not exists")
 
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+
+    os.mkdir("./public")
+
+    src = "./static"
+    dest = "./public"
+
+    copy_static(src, dest)
+
+
+def copy_static(src, dest):
+    """ recursively copies static files into public directory"""
+
+    current_src = src
+    current_dest = dest
+    content = os.listdir(current_src)
+
+    for item in content:
+
+        if os.path.isfile(f"{current_src}/{item}"):
+            shutil.copy(f"{current_src}/{item}", f"{current_dest}/{item}")
+        else:
+            os.mkdir(f"{current_dest}/{item}")
+            copy_static(f"{current_src}/{item}", f"{current_dest}/{item}")
 
 main()
